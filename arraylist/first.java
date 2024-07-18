83997732650fcd9ce9af7ddee775a84fcaba9917
Amit Kumar Sharma <cstechcoding@gmail.com>
2024-07-18 13:54:43 +0530

    first commit

---
+++ arraylist/first.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class first{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(3);
+        System.out.println(list);
+    }
+}
